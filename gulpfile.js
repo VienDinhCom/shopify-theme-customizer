82be90fffd5469fa74e2b5114738fcf356c14ba2
Vien Dinh <maxvien@gmail.com>
2021-07-07 14:36:43 +0700

    update gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,45 +1,50 @@
+const fs = require('fs');
 const gulp = require('gulp');
 const envKit = require('env-kit');
-const merge = require('merge-stream');
-const path = require('path');
+const exitHook = require('exit-hook');
 const plugins = require('gulp-load-plugins')();
 const { execSync, spawnSync } = require('child_process');
 
-const { STORE } = envKit.get();
+const Bundler = require('parcel-bundler');
 
-/* Tasks
+exitHook(() => {
+  console.log('Exiting');
+});
+
+/* Theme
 ----------------------------------------------------*/
-const sources = {
-  assets: 'src/assets/*.*',
-  config: 'src/config/*.json',
-  layout: 'src/layout/*.liquid',
-  locales: 'src/locales/*.json',
-  sections: 'src/sections/*.liquid',
-  snippets: 'src/snippets/*.liquid',
-  templates: ['src/templates/**/*.liquid', 'src/templates/**/*.json'],
-};
-
-const tasks = {
-  assets: () => {
-    return gulp.src(sources.assets).pipe(gulp.dest('dist/assets'));
+const theme = {
+  sources: {
+    assets: 'src/assets/*.*',
+    config: 'src/config/*.json',
+    layout: 'src/layout/*.liquid',
+    locales: 'src/locales/*.json',
+    sections: 'src/sections/*.liquid',
+    snippets: 'src/snippets/*.liquid',
+    templates: 'src/templates/**/*.liquid',
   },
-  config: () => {
-    return gulp.src(sources.config).pipe(gulp.dest('dist/config'));
-  },
-  layout: () => {
-    return gulp.src(sources.layout).pipe(gulp.dest('dist/layout'));
-  },
-  locales: () => {
-    return gulp.src(sources.locales).pipe(gulp.dest('dist/locales'));
-  },
-  sections: () => {
-    return gulp.src(sources.sections).pipe(gulp.dest('dist/sections'));
-  },
-  snippets: () => {
-    return gulp.src(sources.snippets).pipe(gulp.dest('dist/snippets'));
-  },
-  templates: () => {
-    return gulp.src(sources.templates).pipe(gulp.dest('dist/templates'));
+  tasks: {
+    assets: () => {
+      return gulp.src(theme.sources.assets).pipe(gulp.dest('dist/assets'));
+    },
+    config: () => {
+      return gulp.src(theme.sources.config).pipe(gulp.dest('dist/config'));
+    },
+    layout: () => {
+      return gulp.src(theme.sources.layout).pipe(gulp.dest('dist/layout'));
+    },
+    locales: () => {
+      return gulp.src(theme.sources.locales).pipe(gulp.dest('dist/locales'));
+    },
+    sections: () => {
+      return gulp.src(theme.sources.sections).pipe(gulp.dest('dist/sections'));
+    },
+    snippets: () => {
+      return gulp.src(theme.sources.snippets).pipe(gulp.dest('dist/snippets'));
+    },
+    templates: () => {
+      return gulp.src(theme.sources.templates).pipe(gulp.dest('dist/templates'));
+    },
   },
 };
 
@@ -48,60 +53,47 @@ const tasks = {
 gulp.task(
   'copy',
   gulp.parallel(
-    tasks.assets,
-    tasks.config,
-    tasks.layout,
-    tasks.locales,
-    tasks.sections,
-    tasks.snippets,
-    tasks.templates
+    theme.tasks.assets,
+    theme.tasks.config,
+    theme.tasks.layout,
+    theme.tasks.locales,
+    theme.tasks.sections,
+    theme.tasks.snippets,
+    theme.tasks.templates
   )
 );
 
 /* Bundle
 ----------------------------------------------------*/
-const Bundler = require('parcel-bundler');
+gulp.task('bundle', async (options) => {
+  const entry = 'src/main.js';
 
-gulp.task('bundle', (options) => {
-  return new Bundler('src/main.js', {
-    outDir: 'dist/assets',
-    hmr: false,
-    watch: false,
-    minify: false,
-    sourceMaps: false,
-    ...options,
-  }).bundle();
+  if (fs.existsSync(entry)) {
+    const bundler = new Bundler(entry, {
+      hmr: false,
+      minify: true,
+      watch: false,
+      sourceMaps: false,
+      outDir: 'dist/assets',
+      ...options,
+    });
+
+    bundler.bundle();
+  }
 });
 
 /* Watch
 ----------------------------------------------------*/
-gulp.task('watch', () => {
-  gulp.task('bundle')({ watch: true });
+gulp.task('watch', async () => {
+  gulp.watch(theme.sources.assets, theme.tasks.assets);
+  gulp.watch(theme.sources.config, theme.tasks.config);
+  gulp.watch(theme.sources.layout, theme.tasks.layout);
+  gulp.watch(theme.sources.locales, theme.tasks.locales);
+  gulp.watch(theme.sources.sections, theme.tasks.sections);
+  gulp.watch(theme.sources.snippets, theme.tasks.snippets);
+  gulp.watch(theme.sources.templates, theme.tasks.templates);
 
-  gulp.watch(sources.assets, tasks.assets);
-  gulp.watch(sources.config, tasks.config);
-  gulp.watch(sources.layout, tasks.layout);
-  gulp.watch(sources.locales, tasks.locales);
-  gulp.watch(sources.sections, tasks.sections);
-  gulp.watch(sources.snippets, tasks.snippets);
-  gulp.watch(sources.templates, tasks.templates);
-});
-
-/* Serve
-----------------------------------------------------*/
-gulp.task('serve', async () => {
-  for (let index = 0; index < 3; index++) {
-    spawnSync('shopify', ['switch', `--store=${STORE}`], {
-      stdio: 'inherit',
-    });
-
-    const { status } = spawnSync('shopify', ['theme', 'serve'], {
-      cwd: 'dist',
-      stdio: 'inherit',
-    });
-
-    if (status === 1) execSync('shopify logout');
-  }
+  gulp.task('bundle')({ minify: false, watch: true });
 });
 
 /* Clean
@@ -110,6 +102,25 @@ gulp.task('clean', () => {
   return gulp.src(['dist/*', '.cache/*']).pipe(plugins.clean({ force: true }));
 });
 
-gulp.task('build', gulp.series('clean', gulp.parallel('copy', 'bundle')));
+gulp.task('serve', gulp.series('clean', 'copy', 'watch'));
 
-gulp.task('dev', gulp.series('clean', 'copy', gulp.parallel('watch', 'serve')));
+gulp.task('build', gulp.series('clean', 'copy', 'bundle'));
+
+/* Serve
+----------------------------------------------------*/
+// gulp.task('serve', async () => {
+//   while (true) {
+//     const { STORE } = envKit.get();
+
+//     spawnSync('shopify', ['switch', `--store=${STORE}`], {
+//       stdio: 'inherit',
+//     });
+
+//     const { status } = spawnSync('shopify', ['theme', 'serve'], {
+//       cwd: 'dist',
+//       stdio: 'inherit',
+//     });
+
+//     if (status === 1) execSync('shopify logout');
+//   }
+// });
