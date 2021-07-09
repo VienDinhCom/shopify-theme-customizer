f3fa2796c65bab8e2aa755424e1496f14bebe76e
Vien Dinh <maxvien@gmail.com>
2021-07-09 15:48:34 +0700

    remove unused packages

--- src/main.js
+++ src/main.js
@@ -5,9 +5,3 @@ import './main.scss';
 if (process.env.NODE_ENV === 'development') {
   document.write('<script src="http://localhost:35729/livereload.js"></script>');
 }
-
-async function hello() {
-  return 1;
-}
-
-hello();
